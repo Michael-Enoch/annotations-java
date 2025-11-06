89d004826b6fd167d0c002bf7aecdf345d615573
Michael <mike.saviour08@gmail.com>
2025-11-06 20:28:34 +0100

    updated constructor parameter

--- src/main/java/com/michael/Cat.java
+++ src/main/java/com/michael/Cat.java
@@ -1,17 +1,21 @@
 package com.michael;
 
+import com.michael.annotation.RunImmediately;
+import com.michael.annotation.VeryImportant;
+
+@VeryImportant
 public class Cat {
     String name;
     int age;
-    public Cat(String name, int age){
+    public Cat(String name){
         this.name = name;
-        this.age = age;
     }
-
+@RunImmediately
     public void meow(){
         System.out.println(this.name + "Meows !");
     }
 
+    @RunImmediately
     public void eat(){
         System.out.println("Munch!");
     }
