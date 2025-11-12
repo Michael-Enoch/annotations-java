802a2a066b71ff94de41ee4d1e5abb355ab68aca
Michael <mike.saviour08@gmail.com>
2025-11-12 15:21:40 +0100

    updated
    Signed-off-by: Michael <mike.saviour08@gmail.com>

--- src/main/java/com/michael/CreatingAnnotations.java
+++ src/main/java/com/michael/CreatingAnnotations.java
@@ -1,12 +1,15 @@
 package com.michael;
 
+import com.michael.annotation.ImportantString;
 import com.michael.annotation.RunImmediately;
 import com.michael.annotation.VeryImportant;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class CreatingAnnotations {
-    static void main(String[] args) {
+    static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         Cat myCat = new Cat("Stella");
 
         if (myCat.getClass().isAnnotationPresent(VeryImportant.class)){
@@ -16,7 +19,22 @@ public class CreatingAnnotations {
         };
 
         for (Method method: myCat.getClass().getDeclaredMethods()){
-            method.isAnnotationPresent(RunImmediately.class);
+            if(method.isAnnotationPresent(RunImmediately.class)){
+                RunImmediately annotation = method.getAnnotation(RunImmediately.class);
+                for (int i = 0; i < annotation.times(); i++) {
+                    method.invoke(myCat);
+                }
+            };
+        }
+
+        for (Field field: myCat.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(ImportantString.class)){
+                Object o = field.get(myCat);
+
+                if (o instanceof String stringValue){
+                    System.out.println(stringValue.toUpperCase());
+                }
+            }
         }
     }
 }
