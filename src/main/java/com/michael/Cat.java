692c45b1ebded56d7ae3a55bb7d2b3a9ab57181a
Michael <mike.saviour08@gmail.com>
2025-11-12 15:21:58 +0100

    annotated a field
    Signed-off-by: Michael <mike.saviour08@gmail.com>

--- src/main/java/com/michael/Cat.java
+++ src/main/java/com/michael/Cat.java
@@ -1,21 +1,22 @@
 package com.michael;
 
+import com.michael.annotation.ImportantString;
 import com.michael.annotation.RunImmediately;
 import com.michael.annotation.VeryImportant;
 
 @VeryImportant
 public class Cat {
+    @ImportantString
     String name;
     int age;
     public Cat(String name){
         this.name = name;
     }
-    @RunImmediately
+    @RunImmediately( times = 3)
     public void meow(){
-        System.out.println(this.name + "Meows !");
+        System.out.println(this.name + " Meows !");
     }
 
-    @RunImmediately
     public void eat(){
         System.out.println("Munch!");
     }
